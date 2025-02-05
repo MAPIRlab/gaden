883b9ee6d3bcde05351801499bcb048af256dc38
Pepe Ojeda <ojedamorala@hotmail.com>
2025-02-05 14:46:37 +0100

    added a version of read_sim_yaml that takes the path explicitly

--- gaden_common/gaden_internal_py/utils.py
+++ gaden_common/gaden_internal_py/utils.py
@@ -7,12 +7,15 @@ import yaml
 # Loads the custom Gaden simulation YAML and creates a LaunchConfiguration for each key
 # You can use ROS substitutions inside of the simulation YAML, too
 # However, it is a bad idea to use a substitution with a configuration that is defined in that same file, as the order in which the yaml keys are loaded is not guaranteed
-def read_sim_yaml(context):
-    with open(os.path.join(LaunchConfiguration("pkg_dir").perform(context), 
-                           "scenarios",
-                            LaunchConfiguration("scenario").perform(context),
-                            "simulations", 
-                            LaunchConfiguration("simulation").perform(context)+".yaml")) as yaml_stream:
+def read_sim_yaml(context, path):
+    with open(path) as yaml_stream:
         sim_description = yaml.load(yaml_stream, Loader=yaml.FullLoader)
         for key in sim_description:
             SetLaunchConfiguration(name=key, value=parse_substitution(str(sim_description[key]))).execute(context)
+
+def read_sim_yaml(context):
+    read_sim_yaml(os.path.join(LaunchConfiguration("pkg_dir").perform(context), 
+                           "scenarios",
+                            LaunchConfiguration("scenario").perform(context),
+                            "simulations", 
+                            LaunchConfiguration("simulation").perform(context)+".yaml"))
