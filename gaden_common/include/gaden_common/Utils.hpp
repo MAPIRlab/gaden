fdd0eb59e250b8d8c73c38fe889e4dda5eaaad2a
Pepe Ojeda <ojedamorala@hotmail.com>
2025-05-23 10:40:00 +0200

    added gaden_core and changed preprocessing to use it

--- gaden_common/include/gaden_common/Utils.hpp
+++ gaden_common/include/gaden_common/Utils.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <filesystem>
 #include <rclcpp/rclcpp.hpp>
 
 template <typename T> T getParam(rclcpp::Node::SharedPtr node, const std::string& name, T defaultValue)
@@ -7,4 +8,13 @@ template <typename T> T getParam(rclcpp::Node::SharedPtr node, const std::string
         return node->get_parameter_or<T>(name, defaultValue);
     else
         return node->declare_parameter<T>(name, defaultValue);
+}
+
+inline std::vector<std::filesystem::path> AsPaths(const std::vector<std::string>& strs)
+{
+    std::vector<std::filesystem::path> paths;
+    paths.reserve(strs.size());
+    for (const auto& str : strs)
+        paths.emplace_back(str);
+    return paths;
 }
